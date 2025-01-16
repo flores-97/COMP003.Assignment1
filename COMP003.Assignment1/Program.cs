7580534515575db143629ad17d747c3c0adf2c5a
flores-97 <floresvictor15@gmail.com>
2025-01-15 20:35:59 -0800

    added author, course, etc.

--- COMP003.Assignment1/Program.cs
+++ COMP003.Assignment1/Program.cs
@@ -9,10 +9,15 @@ namespace COMP003.Assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your name: Victor.");
+            Console.WriteLine("Enter your name: Flores-97.");
             Console.WriteLine("Hello, FLores-97! Your name is Victor Flores.");
-
-            Console.WriteLine("Here are tem things about me:");
+           
+            Console.WriteLine("Here are tem things about me:" +
+                "\n1. I like to watch Star Wars." +
+                "\n2. I enjoy building Lego Star Wars and have built over 15." +
+                "\n3. I have traveled to three countries already." +
+                "\n4. I have two dogs, one maltipoo and one shit tzu." +
+                "\n5. My wife and I will be celebrating our two year anniversary at the beginning of March. ");
         }
     }
 }

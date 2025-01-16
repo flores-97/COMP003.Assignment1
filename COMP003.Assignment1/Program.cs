42b43ea6b84f3e10894a4f03f08c162a38a09453
flores-97 <floresvictor15@gmail.com>
2025-01-15 20:45:20 -0800

    completed all 10 items

--- COMP003.Assignment1/Program.cs
+++ COMP003.Assignment1/Program.cs
@@ -9,15 +9,21 @@ namespace COMP003.Assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your name: Flores-97.");
-            Console.WriteLine("Hello, FLores-97! Your name is Victor Flores.");
+            Console.WriteLine("Enter your name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Hello " + username + ". My name is Victor");
            
-            Console.WriteLine("Here are tem things about me:" +
-                "\n1. I like to watch Star Wars." +
-                "\n2. I enjoy building Lego Star Wars and have built over 15." +
-                "\n3. I have traveled to three countries already." +
-                "\n4. I have two dogs, one maltipoo and one shit tzu." +
-                "\n5. My wife and I will be celebrating our two year anniversary at the beginning of March. ");
+            Console.WriteLine("Here are ten things about me:" +
+                "\n\t1. I like to watch Star Wars." +
+                "\n\t2. I enjoy building Lego Star Wars and have built over 15." +
+                "\n\t3. I have traveled to three countries already." +
+                "\n\t4. I have two dogs, one maltipoo and one shit tzu." +
+                "\n\t5. My wife and I will be celebrating our two year anniversary at the beginning of March. " +
+                "\n\t6. I have been playing soccer for over 12 years. " +
+                "\n\t7. My favorite soccer team is Manchester United. " +
+                "\n\t8. My favorite soccer cleat is the Nike Tiempo and the Nike Total 90." +
+                "\n\t9. I have been in the US Navy for 9 years. " +
+                "\n\t10. My contract will end in two years.");
         }
     }
 }

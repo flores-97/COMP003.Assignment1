19fc2a5e336082a04cb7c99ed3e3db2c47792922
flores-97 <floresvictor15@gmail.com>
2025-01-15 20:26:19 -0800

    Add project files.

---
+++ COMP003.Assignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Author:Victor Flores
+ *Course: COMP003A
+ *Faculty: Jonathon Cruz
+ *Purpose: ASsigment 1
+ */
+namespace COMP003.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: Victor.");
+            Console.WriteLine("Hello, FLores-97! Your name is Victor Flores.");
+
+            Console.WriteLine("Here are tem things about me:");
+        }
+    }
+}
